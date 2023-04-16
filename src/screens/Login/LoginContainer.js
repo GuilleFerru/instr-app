74d2fb02bd3fba3d151086c2db05c33ab6e6e219
GuilleFerru <guillelf@gmail.com>
2023-04-16 20:27:34 -0300

    fix endDate and endDateTime in completeDailyRoutine

--- src/screens/Login/LoginContainer.js
+++ src/screens/Login/LoginContainer.js
@@ -58,7 +58,6 @@ export const Login = () => {
                                 autoFocus={true}
                                 type={"email"}
                                 inputRef={username}
-                                value={'gferrucci@pr3.com.ar'}
                             />
                             <Input
                                 variant={"outlined"}
@@ -71,7 +70,6 @@ export const Login = () => {
                                 autoComplete={"current-password"}
                                 type={"password"}
                                 inputRef={password}
-                                value={'1492'}
                             />
                             <Button
                                 type="submit"
